7e2994c3aec63dbe7c9204899121497166ef9c43
nilzxq <2013358717@qq.com>
2016-11-26 21:08:08 +0800

    @ResonseBody JSON

--- SpringMVC-HelloWorld/src/com/nilzxq/entity/AddressEntity.java
+++ SpringMVC-HelloWorld/src/com/nilzxq/entity/AddressEntity.java
@@ -1,14 +1,19 @@
 package com.nilzxq.entity;
 
 public class AddressEntity {
-  private String city;
+	private String city;
 
-public String getCity() {
-	return city;
-}
+	public AddressEntity(String city) {
+		super();
+		this.city = city;
+	}
 
-public void setCity(String city) {
-	this.city = city;
-}
-  
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
 }
